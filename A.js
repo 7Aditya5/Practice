a82632f34cc9a2e367e1ed4e84bb3422f450224d
Aditya <aditya1998sadhukhan@gmail.com>
2022-11-08 02:34:38 +0530

    pushing the practice files

---
+++ A.js
@@ -0,0 +1,7 @@
+let cont = document.getElementById("container");
+
+function changeSizeBySlider() {
+    let slider = document.getElementById("slider");
+   
+    cont.style.fontSize = slider.value+"em";
+  }
