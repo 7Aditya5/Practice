b0dbe9699188d8e9c338e42b0357c07a51c58c4f
Aditya <aditya1998sadhukhan@gmail.com>
2022-11-08 15:14:46 +0530

    adding zoomin with space key

--- A.js
+++ A.js
@@ -1,8 +1,15 @@
 let cont = document.getElementById("container");
 
-
-function changeSizeBySlider() {
-    let slider = document.getElementById("slider");
-   
-    cont.style.fontSize = slider.value+"em";
-  }
+    document.addEventListener("keypress", function onPress(event) {
+      if (event.key === " ") {
+        let style = window.getComputedStyle(cont, null).getPropertyValue('font-size');
+        let currentSize = parseInt(style);
+        console.log(currentSize);
+        cont.style.fontSize = (currentSize * 2)+'px';
+        //console.log(cont.style.fontSize);
+      }
+      if(event.key === 'r'){
+        cont.style.color = "red";
+      }
+  });
+  
